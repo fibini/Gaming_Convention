e6610dc61d45b01b398cc200e683990b2b2fb532
Fabien <fabienswift@gmail.com>
2022-05-19 19:06:33 -0400

    Add java.js for about.html so hamburger button can work

---
+++ java.js
@@ -0,0 +1,12 @@
+const hamburgericon = document.querySelector('.hamburger-button');
+const navigationBar = document.querySelector('.nav-bars');
+
+hamburgericon.addEventListener('click', () =>{
+    hamburgericon.classList.toggle('show');
+    navigationBar.classList.toggle('show')
+});
+
+document.querySelectorAll('.nav-bars a').forEach((n) => n.addEventListener('click', () => {
+    hamburgericon.classList.remove('show');
+    navigationBar.classList.remove('show');
+  }));
