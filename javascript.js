95625ffb029ae41d0679a2014ce830fbb5b75711
Fabien <fabienswift@gmail.com>
2022-05-21 09:21:59 -0400

    Made required changes to css of about and index.html

--- javascript.js
+++ javascript.js
@@ -54,14 +54,14 @@ features.forEach((feature) => {
 });
 
 const hamburgericon = document.querySelector('.hamburger-button');
-const navigationBar = document.querySelector('.nav-bars');
+const navigationBar = document.querySelector('.menu-nav');
 
 hamburgericon.addEventListener('click', () => {
   hamburgericon.classList.toggle('show');
   navigationBar.classList.toggle('show');
 });
 
-document.querySelectorAll('.nav-bars a').forEach((n) => n.addEventListener('click', () => {
+document.querySelectorAll('.menu-nav a').forEach((n) => n.addEventListener('click', () => {
   hamburgericon.classList.remove('show');
   navigationBar.classList.remove('show');
 }));
